94615282031e94e26ba9ee6ef4eb098fbf9ea88a
Devesh Chaudhary <devesh.chaudhary.30994@gmail.com>
2020-04-04 14:42:42 +0530

    c

---
+++ project/app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
