07652b44b8a5d7c5c35d0275cc95f2cdb31220b4
Devesh Chaudhary <devesh.chaudhary.30994@gmail.com>
2020-04-04 14:58:42 +0530

    c

---
+++ project/resources/views/layouts/adminLayout/admin_footer.blade.php
@@ -0,0 +1,5 @@
+
+<div class="row-fluid">
+    <div id="footer" class="span12"> 2020 &copy; Shopping-Website Admin </div>
+  </div>
+  
---
+++ project/resources/views/layouts/adminLayout/admin_header.blade.php
@@ -0,0 +1,12 @@
+<div id="header">
+  <h1><a href="{{ url('/admin/dashboard') }}">Laravel Admin</a></h1>
+</div>
+
+
+<div id="user-nav" class="navbar navbar-inverse">
+  <ul class="nav">
+  <li class=""><a title="" href="{{url('/admin/settings')}}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
+    <li class=""><a title="" href="{{url('/logout')}}"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+    <li class=""><a title="" href="{{url('/')}}"><i class="icon icon-site"></i> <span class="text">Visit Site</span></a></li>
+  </ul>
+</div>
